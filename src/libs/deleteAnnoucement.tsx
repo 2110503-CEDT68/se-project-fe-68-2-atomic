9395145f33ab83fa056fe1c6bb7849c407cfe32b
thisisrealcd <cdzaa9850@gmail.com>
2026-04-11 21:33:43 +0700

    Create libs for delete Announcement

---
+++ src/libs/deleteAnnoucement.tsx
@@ -0,0 +1,21 @@
+'use server'
+import { revalidateTag } from 'next/cache'
+export default async function deleteAnnouncement(
+	aid: string,  
+    token: string
+){
+   
+    const respond = await fetch(`${process.env.BACKEND_URL}/api/announcements/${aid}`, {
+        method: "DELETE",
+		headers:{
+			authorization: `Bearer ${token}`,
+		},
+    })
+    if(!respond.ok){
+        const errText = await respond.text()
+        throw new Error(`Failed to delete Announcement: ${respond.status} - ${errText}`)
+    }
+    revalidateTag('announcements')
+    revalidateTag(`announcement-${aid}`)
+    return await respond.json()
+}
